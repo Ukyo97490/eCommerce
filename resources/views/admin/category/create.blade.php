76a09e656e4c28a2f101f90363c3875405063b5c
Ukyo97490 <damour.gregory@gmail.com>
2022-07-21 13:18:03 +0400

    Modification du pannel admin

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,79 +1,79 @@
 @extends('layouts.admin')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Ajouter une Catégorie</h4>
+                    <a href="{{ url('admin/category/create') }}"
+                        class="btn btn-primary btn-sm text-white float-end">Retour</a>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('admin/category') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 mb-3">
+                                <label>Nom*</label>
+                                <input type="text" name="name" class="form-control" />
+                                @error('name')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Slug*</label>
+                                <input type="text" name="slug" class="form-control" />
+                                @error('slug')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label>Description*</label>
+                                <textarea name="description" class="form-control" rows="3"></textarea>
+                                @error('description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Image</label>
+                                <input type="file" name="image" class="form-control" />
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <label>Statut</label><br />
+                                <input type="checkbox" name="status" />
+                            </div>
 
-<div class="row">
-    <div class="col-md-12 grid-margin">
-        <div class="card">
-            <div class="card-header">
-                <h4>Créer Categories</h4>
-                <a href="{{ url('admin/category/create') }}" class="btn btn-primary btn-sm text-white float-end">Retour</a>
-            </div>
-            <div class="card-body">
-                <form action="{{url('admin/category')}}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label>Nom*</label>
-                            <input type="text" name="name" class="form-control" />
-                            @error('name')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label>Slug*</label>
-                            <input type="text" name="slug" class="form-control" />
-                            @error('slug')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label>Description*</label>
-                            <textarea name="description" class="form-control" rows="3"></textarea>
-                            @error('description')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label>Image</label>
-                            <input type="file" name="image" class="form-control" />
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label>Statut</label><br />
-                            <input type="checkbox" name="status" />
-                        </div>
+                            <div class="col-md-12">
+                                <h4>SEO Meta</h4>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label>Meta Title*</label>
+                                <input type="text" name="meta_title" class="form-control" />
+                                @error('meta_title')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label>Meta Keyword*</label>
+                                <textarea name="meta_keyword" class="form-control" rows="3"></textarea>
+                                @error('meta_keyword')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <label>Meta Description*</label>
+                                <textarea name="meta_description" class="form-control" rows="3"></textarea>
+                                @error('meta_description')
+                                    <small class="text-danger">{{ $message }}</small>
+                                @enderror
+                            </div>
 
-                        <div class="col-md-12">
-                            <h4>SEO Meta</h4>
+                            <div class="col-md-12 mb-3">
+                                <button type="submit" class="btn btn-primary float-end text-white">Enregistrer</button>
+                            </div>
                         </div>
-                        <div class="col-md-12 mb-3">
-                            <label>Meta Title*</label>
-                            <input type="text" name="meta_title" class="form-control" />
-                            @error('meta_title')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label>Meta Keyword*</label>
-                            <textarea name="meta_keyword" class="form-control" rows="3"></textarea>
-                            @error('meta_keyword')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label>Meta Description*</label>
-                            <textarea name="meta_description" class="form-control" rows="3"></textarea>
-                            @error('meta_description')
-                            <small class="text-danger">{{$message}}</small>
-                            @enderror
-                        </div>
-
-                        <div class="col-md-12 mb-3">
-                            <button type="submit" class="btn btn-primary float-end text-white">Enregistrer</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
     @endsection
--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -1,79 +1,85 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
-  <ul class="nav">
-    <li class="nav-item">
-      <a class="nav-link" href="index.html">
-        <i class="mdi mdi-home menu-icon"></i>
-        <span class="menu-title">Panneau d'Administration</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="pages/forms/basic_elements.html">
-        <i class="mdi mdi-view-headline menu-icon"></i>
-        <span class="menu-title">Sales</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#category" aria-expanded="false" aria-controls="category">
-        <i class="mdi mdi-circle-outline menu-icon"></i>
-        <span class="menu-title">Catégories</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="category">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="#">Lien 1</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Lien 2</a></li>
-        </ul>
-      </div>
-    </li>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+                <i class="mdi mdi-home menu-icon"></i>
+                <span class="menu-title">Panneau d'Administration</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/forms/basic_elements.html">
+                <i class="mdi mdi-view-headline menu-icon"></i>
+                <span class="menu-title">Sales</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#category" aria-expanded="false"
+                aria-controls="category">
+                <i class="mdi mdi-circle-outline menu-icon"></i>
+                <span class="menu-title">Catégories</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="category">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category/create') }}">Ajouter une
+                            catégorie</a>
+                    </li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('admin/category') }}">Voir les
+                            catégories</a></li>
+                </ul>
+            </div>
+        </li>
 
 
 
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#products" aria-expanded="false" aria-controls="products">
-        <i class="mdi mdi-circle-outline menu-icon"></i>
-        <span class="menu-title">Products</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="products">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="#">Lien 1</a></li>
-          <li class="nav-item"> <a class="nav-link" href="#">Lien 2</a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="pages/forms/basic_elements.html">
-        <i class="mdi mdi-view-headline menu-icon"></i>
-        <span class="menu-title">Brands</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-        <i class="mdi mdi-account menu-icon"></i>
-        <span class="menu-title">Users</span>
-        <i class="menu-arrow"></i>
-      </a>
-      <div class="collapse" id="auth">
-        <ul class="nav flex-column sub-menu">
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
-          <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
-        </ul>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="documentation/documentation.html">
-        <i class="mdi mdi-file-document-box-outline menu-icon"></i>
-        <span class="menu-title">Home Slider</span>
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="documentation/documentation.html">
-        <i class="mdi mdi-file-document-box-outline menu-icon"></i>
-        <span class="menu-title">Site Setting</span>
-      </a>
-    </li>
-  </ul>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#products" aria-expanded="false"
+                aria-controls="products">
+                <i class="mdi mdi-circle-outline menu-icon"></i>
+                <span class="menu-title">Products</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="products">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="#">Lien 1</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="#">Lien 2</a></li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/forms/basic_elements.html">
+                <i class="mdi mdi-view-headline menu-icon"></i>
+                <span class="menu-title">Marques</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i class="mdi mdi-account menu-icon"></i>
+                <span class="menu-title">Users</span>
+                <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a>
+                    </li>
+                </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="documentation/documentation.html">
+                <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+                <span class="menu-title">Home Slider</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="documentation/documentation.html">
+                <i class="mdi mdi-file-document-box-outline menu-icon"></i>
+                <span class="menu-title">Site Setting</span>
+            </a>
+        </li>
+    </ul>
 </nav>
